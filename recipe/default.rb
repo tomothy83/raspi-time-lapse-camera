0fae1d54e7d702e84f0aeb2c9e83946d54456b25
Tomoaki Yasaka <34957299+tomothy83@users.noreply.github.com>
2021-02-28 08:35:19 +0000

    Add script.

--- recipe/default.rb
+++ recipe/default.rb
@@ -1,3 +1,9 @@
+node.validate! do
+  {
+    image_resolution: string,
+  }
+end
+
 appdir = "/opt/time-lapse-camera"
 u = "timelapsecam"
 
@@ -8,6 +14,7 @@ directory appdir do
 end
 
 package "fswebcam"
+package "v4l-utils"
 
 user u do
   home appdir
@@ -16,4 +23,16 @@ end
 
 execute "usermod #{u} -aG video" do
   not_if "id -nG #{u} | grep -w video"
+end
+
+directory "#{appdir}/bin" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+remote_file "#{appdir}/bin/TAKE_PICTURE" do
+  owner "root"
+  group "root"
+  mode "0755"
 end
